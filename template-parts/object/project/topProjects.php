becf2bbbb5c9a8b3b95dd54c96979d1d3399507f
tanaka_anji <tanaka_anji@moltsinc.co.jp>
2025-03-05 11:39:52 +0900

    feature: pc版実装

--- template-parts/object/project/topProjects.php
+++ template-parts/object/project/topProjects.php
@@ -11,5 +11,49 @@
         </button>
       </div>
     </div>
+    <div class="p-topProjects__list">
+      <div class="p-topProjects__item">
+        <div class="p-topProjects__content">
+          <p class="p-topProjects__subtitle">
+            新コンセプトが、顧客、営業、社内を変え事業が加速
+          </p>
+          <p class="p-topProjects__title">
+            アントレの新商品開発の問題とリリースまでのプロセス
+          </p>
+          <div class="p-topProjects__main">
+            <p class="p-topProjects__description">
+              THE MOLTISは専門性の異なるプロフェッショナルが集まっているため、幅広いデジタルマーケティング領域に対応することができます。
+              特定の手段にとらわれず、様々な課題を解決し、最適な戦略・施策・体制によるデジタルマーケティングの実施により、堅実な事業成長を実現します。
+            </p>
+            <a href="#" class="p-topProjects__link">詳細を見る</a>
+          </div>
+          <div class="p-topProjects__tags">
+            <a href="#" class="p-topProjects__tag">戦略/ブランディング</a>
+            <a href="#" class="p-topProjects__tag">コンテンツSEO</a>
+            <a href="#" class="p-topProjects__tag">コミュニケーションプランニング</a>
+          </div>
+          <div class="p-topProjects__comment">
+            <p class="p-topProjects__commentText">
+              設計部分の〇〇をいち早く注入することで、初速の立ち上がりが良かったと思いました。
+              〇〇さんの対応も素晴らしく、素晴らしい結果が出せて最高でした。
+            </p>
+            <div class="p-topProjects__author">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/demo/user.png" alt="幸倉 大生" width="24" height="24">
+              <span>
+                担当者<a href="#" class="c-underlineLink">幸倉 大生</a>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="p-topProjects__images">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/demo/thumbnail1.png" alt="メイン画像" class="p-topProjects__mainImage">
+          <div class="p-topProjects__thumbnails">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/demo/thumbnail1.png" alt="サムネイル1" class="p-topProjects__thumbnail">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/demo/thumbnail1.png" alt="サムネイル2" class="p-topProjects__thumbnail">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/demo/thumbnail1.png" alt="サムネイル3" class="p-topProjects__thumbnail">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </section>
